80f7407e1a0b44eedbedfd633ecb7ec489025829
sipal <sipal.s.ramadhan>
2022-02-28 18:09:07 +0300

    add tasks

--- lib/addTasks/addTasks.dart
+++ lib/addTasks/addTasks.dart
@@ -57,7 +57,60 @@ class _AddTasksState extends State<AddTasks> {
         btnColor: kRed,
         left: 0,
         done: 10,
-        desc: [],
+        desc: [
+          {
+            "time": "09:00",
+            "title": " something ",
+            "slot": "09:00 - 10:00",
+            "tlColor": kBlueDark,
+            "bgColor": kBlueLight,
+          },
+          {
+            "time": "11:00",
+            "title": "Work at Computer",
+            "slot": "11:00 - 12:00",
+            "tlColor": kYellowDark,
+            "bgColor": kYellowLight,
+          },
+          {
+            "time": "13:00",
+            "title": "  ",
+            "slot": " ",
+            "tlColor": Colors.grey.withOpacity(0.1)
+          },
+          {
+            "time": "14:00",
+            "title": "Eat Something",
+            "slot": "14:00 - 15:00",
+            "tlColor": kRedDark,
+            "bgColor": kRedLight,
+          },
+          {
+            "time": "15:00",
+            "title": "  ",
+            "slot": " ",
+            "tlColor": Colors.grey.withOpacity(0.1)
+          },
+          {
+            "time": "16:00",
+            "title": "  ",
+            "slot": " ",
+            "tlColor": Colors.grey.withOpacity(0.1)
+          },
+          {
+            "time": "17:00",
+            "title": "  ",
+            "slot": " ",
+            "tlColor": Colors.grey.withOpacity(0.1)
+          },
+          {
+            "time": "18:00",
+            "title": "Use social media",
+            "slot": "18:00 - 19:00",
+            "tlColor": kRedDark,
+            "bgColor": kRedLight,
+          },
+        ],
         createdTime: DateTime.now(),
       );
       final provider = Provider.of<TasksProvider>(context, listen: false);
