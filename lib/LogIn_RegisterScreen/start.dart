fd680d011da4bb324efb1edd4ffea04c5e3aec59
Dilman <48926040+delmanAli@users.noreply.github.com>
2022-03-02 11:47:38 +0300

    fix login

--- lib/LogIn_RegisterScreen/start.dart
+++ lib/LogIn_RegisterScreen/start.dart
@@ -3,44 +3,39 @@ import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-class Start extends StatefulWidget {
-  @override
-  _StartState createState() => _StartState();
-}
+class Start extends StatelessWidget {
+  // final FirebaseAuth _auth = FirebaseAuth.instance;
 
-class _StartState extends State<Start> {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  // Future<UserCredential> googleSignIn() async {
+  //   GoogleSignIn googleSignIn = GoogleSignIn();
+  //   GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+  //   if (googleUser != null) {
+  //     GoogleSignInAuthentication googleAuth = await googleUser.authentication;
 
-  Future<UserCredential> googleSignIn() async {
-    GoogleSignIn googleSignIn = GoogleSignIn();
-    GoogleSignInAccount? googleUser = await googleSignIn.signIn();
-    if (googleUser != null) {
-      GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+  //     if (googleAuth.idToken != null && googleAuth.accessToken != null) {
+  //       final AuthCredential credential = GoogleAuthProvider.credential(
+  //           accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
 
-      if (googleAuth.idToken != null && googleAuth.accessToken != null) {
-        final AuthCredential credential = GoogleAuthProvider.credential(
-            accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
+  //       final UserCredential user =
+  //           await _auth.signInWithCredential(credential);
 
-        final UserCredential user =
-            await _auth.signInWithCredential(credential);
+  //       await Navigator.pushReplacementNamed(context, "/");
 
-        await Navigator.pushReplacementNamed(context, "/");
+  //       return user;
+  //     } else {
+  //       throw StateError('Missing Google Auth Token');
+  //     }
+  //   } else
+  //     throw StateError('Sign in Aborted');
+  // }
 
-        return user;
-      } else {
-        throw StateError('Missing Google Auth Token');
-      }
-    } else
-      throw StateError('Sign in Aborted');
-  }
+  // navigateToLogin() async {
+  //   Navigator.pushReplacementNamed(context, "/Login");
+  // }
 
-  navigateToLogin() async {
-    Navigator.pushReplacementNamed(context, "/Login");
-  }
-
-  navigateToRegister() async {
-    Navigator.pushReplacementNamed(context, "/SignUp");
-  }
+  // navigateToRegister() async {
+  //   Navigator.pushReplacementNamed(context, "/SignUp");
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -80,7 +75,9 @@ class _StartState extends State<Start> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 ElevatedButton(
-                  onPressed: navigateToLogin,
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/Login');
+                  },
                   child: const Text('LOGIN'),
                   style: ElevatedButton.styleFrom(
                       primary: Colors.black,
@@ -93,7 +90,9 @@ class _StartState extends State<Start> {
                 ),
                 const SizedBox(width: 20.0),
                 ElevatedButton(
-                  onPressed: navigateToRegister,
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/SignUp');
+                  },
                   child: const Text('SIGNUP'),
                   style: ElevatedButton.styleFrom(
                       primary: Colors.black,
@@ -107,8 +106,8 @@ class _StartState extends State<Start> {
               ],
             ),
             const SizedBox(height: 40.0),
-            SignInButton(Buttons.Google,
-                text: "Sign up with Google", onPressed: googleSignIn)
+            // SignInButton(Buttons.Google,
+            //     text: "Sign up with Google", onPressed: googleSignIn)
           ],
         ),
       ),
