ac216170bf1545d21de9e6d05a47db93d4563bf6
amad <amad.atroshi96@gmail.com>
2022-03-03 16:42:35 +0300

    changed theme to light and added screenshots

--- lib/LogIn_RegisterScreen/Login.dart
+++ lib/LogIn_RegisterScreen/Login.dart
@@ -122,7 +122,7 @@ class _LoginState extends State<Login> {
                                   email: email.text, password: pass.text);
 
                               Navigator.pushReplacementNamed(
-                                  context, '/Indexindex');
+                                  context, '/LogedIN');
                             }
                           },
                           child: const Text('LOGIN'),
